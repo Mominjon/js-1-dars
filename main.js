6874a8a148e0c8ac78bc0392c609b7070f63cb62
Mominjon <qodirovumidjo@gmail.com>
2021-04-16 15:30:02 +0500

    l

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var jammi_summa =Number(borish_uchun_dollar+mehmon_hona_uchun+aylanish_uchun_eur
 var korinish = prompt(`alisher senda qancha pul bor agar senda ${jammi_summa} so'm  bolsa hisoblab otirma yolga otlanaver`);
 var kop =(`alisher senda ${korinish} so'm pul bor ekan bemalol yo'lga otlan chunki senga ${jammi_summa} so'm pul kerak holos senda yana ${korinish - jammi_summa} som pul ortib qolyabdi oq yol alisher`);
 var kam =(`alisher sen ozgina sabir qil puling kamroq ekan senga yana ${jammi_summa - korinish} so'm kerak yana ozginia sabir qil`);
-console.log =(`alisher senda qancha pul bor`);
 if (korinish > jammi_summa){
     alert(kop);
 }
